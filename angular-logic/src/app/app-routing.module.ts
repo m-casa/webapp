34cb5228b8df936e5be3fe7d77c711c5cc5adcf8
Marc Casanova <marc.casanova@yahoo.com>
2021-04-19 11:08:20 -0500

    Added two news components and their routing

--- angular-logic/src/app/app-routing.module.ts
+++ angular-logic/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddNewsComponent } from './components/news/add-news/add-news.component';
+import { ListNewsComponent } from './components/news/list-news/list-news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "add-news",
+    component: AddNewsComponent
+  },
+  {
+    path: "list-news",
+    component: ListNewsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- angular-logic/src/app/app.module.ts
+++ angular-logic/src/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { SportsComponent } from './components/sports/sports.component';
-import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { NewsComponent } from './components/news/news.component';
-import { ChatComponent } from './components/chat/chat.component';
+import { AddNewsComponent } from './components/news/add-news/add-news.component';
+import { ListNewsComponent } from './components/news/list-news/list-news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SportsComponent,
-    AboutComponent,
-    ContactComponent,
-    NewsComponent,
-    ChatComponent
+    AddNewsComponent,
+    ListNewsComponent
   ],
   imports: [
     BrowserModule,
