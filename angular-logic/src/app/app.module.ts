c4ba928ce81f95df4f5d516b76a2466fd64f2eb0
an phan <a.phan97@yahoo.com>
2021-04-20 14:19:38 -0500

    declare components

--- angular-logic/src/app/app.module.ts
+++ angular-logic/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { SportsComponent } from './components/sports/sports.component';
 import { AddSportsNewsComponent } from './components/add-sports-news/add-sports-news.component';
 import { AddNewsComponent } from './components/news/add-news/add-news.component';
 import { ListNewsComponent } from './components/news/list-news/list-news.component';
+import { ContactComponent } from './components/contact/contact.component'
+import { AboutComponent } from './components/about/about.component'
 
 @NgModule({
   declarations: [
@@ -17,12 +20,16 @@ import { ListNewsComponent } from './components/news/list-news/list-news.compone
     SportsComponent,
     AddSportsNewsComponent,
     AddNewsComponent,
-    ListNewsComponent
+    ListNewsComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
