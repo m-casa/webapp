677e0fad3331f2445eb83f8adf7606385dfbfc46
an phan <a.phan97@yahoo.com>
2021-04-20 14:17:07 -0500

    added routes

--- angular-logic/src/app/app-routing.module.ts
+++ angular-logic/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddNewsComponent } from './components/news/add-news/add-news.component';
 import { ListNewsComponent } from './components/news/list-news/list-news.component';
+import { ContactComponent } from './components/contact/contact.component'
+import { AboutComponent } from './components/about/about.component'
 
 const routes: Routes = [
   {
@@ -11,6 +13,14 @@ const routes: Routes = [
   {
     path: "list-news",
     component: ListNewsComponent
+  },
+  {
+    path: 'contact',
+  component: ContactComponent
+  },
+  {
+    path: 'about',
+  component: AboutComponent
   }
 ];
 
