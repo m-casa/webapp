f0fc5fc529011de660cd4a44d229c8e024933576
Nicholas Mills <nicholas_mills@outlook.com>
2021-04-20 11:46:23 -0500

    Added error checking in add-sports-news html

--- angular-logic/src/app/components/add-sports-news/add-sports-news.component.ts
+++ angular-logic/src/app/components/add-sports-news/add-sports-news.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ISports } from 'src/app/interfaces/sports';
 import { SportsService } from 'src/app/services/sports.service';
 import { SportsComponent } from '../sports/sports.component';
+import { ViewEncapsulation } from '@angular/core'
 
 @Component({
   selector: 'app-add-sports-news',
   templateUrl: './add-sports-news.component.html',
-  styleUrls: ['./add-sports-news.component.scss']
+  styleUrls: ['./add-sports-news.component.scss'],
+  encapsulation : ViewEncapsulation.None
 })
 export class AddSportsNewsComponent implements OnInit {
 
